357a2353a1d3fe0251a4e024d075c3435dda03c1
jpatrickkelley <jpatrickkelly205@gmail.com>
2019-10-01 08:07:04 -0500

    weekly challenges 2

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+	
 
+	
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
+
+
+
             throw new NotImplementedException();
         }
-
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
             throw new NotImplementedException();
@@ -47,7 +52,20 @@ namespace ChallengesWithTestsMark8
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
+            //string originalString = new string("  ,  ");
+
+            //string[] x = originalString.Split("  ")
+            //.Select(y => y.Substring(0, 1).ToUpper() + y.Substring(1))
+            //.ToArray();
+            // {
+            //Console.WriteLine(words);
+
+            //}     
+
             throw new NotImplementedException();
+
+
+            //throw new NotImplementedException();
         }
     }
 }
